258aa838b02a131a97dbc2e859452c1fa097ab65
Maryam Zakani <zakani91@gmail.com>
2018-12-13 17:45:31 +0100

    Add Europe countries - flags

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,23 +1,36 @@
 $(document).ready(function () {
     $.getJSON('../data/countries_flags.json', function (json) {
-        var categories = json.categories;
-        for (var i = 0; i < categories.length; i++) {
 
+        var categories_switzerland = json.Switzerland.categories;
+        for (var i = 0; i < categories_switzerland.length; i++) {
             var new_row = "";
+            new_row += "<tr><td style='text-align: center'>" + categories_switzerland[i].category_name + "</td>";
 
-            new_row += "<tr><td style='text-align: center'>" + categories[i].category_name + "</td>";
-
-            for (var j = 0; j < categories[i].countries.length; j++) {
+            for (var j = 0; j < categories_switzerland[i].countries.length; j++) {
                 new_row += "<td style='text-align: center'>" +
-                    "<img src=" + categories[i].countries[j].icon + " alt=" + categories[i].countries[j].name + " width='50' height='50' uk-tooltip='" +
-                    categories[i].countries[j].amount + "'>" +
-                    "<p>" + categories[i].countries[j].name + "</p>"
+                    "<img src=" + categories_switzerland[i].countries[j].icon + " alt=" + categories_switzerland[i].countries[j].name + " width='50' height='50' uk-tooltip='" +
+                    categories_switzerland[i].countries[j].amount + "'>" +
+                    "<p>" + categories_switzerland[i].countries[j].name + "</p>"
                     + "</td>";
             }
-
             new_row += "</tr>";
-            $("#swiss-importer-flags").append(new_row)
-            ;
+            $("#swiss-importer-flags").append(new_row);
+        }
+
+        var categories_europe = json.Europe.categories;
+        for (var i = 0; i < categories_europe.length; i++) {
+            var new_row = "";
+            new_row += "<tr><td style='text-align: center'>" + categories_europe[i].category_name + "</td>";
+
+            for (var j = 0; j < categories_europe[i].countries.length; j++) {
+                new_row += "<td style='text-align: center'>" +
+                    "<img src=" + categories_europe[i].countries[j].icon + " alt=" + categories_europe[i].countries[j].name + " width='50' height='50' uk-tooltip='" +
+                    categories_europe[i].countries[j].amount + "'>" +
+                    "<p>" + categories_europe[i].countries[j].name + "</p>"
+                    + "</td>";
+            }
+            new_row += "</tr>";
+            $("#europe-importer-flags").append(new_row);
         }
     });
 });
